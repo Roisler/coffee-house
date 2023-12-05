2c6c14f68693ef6f5d84b176bc4977efe468aeca
Sergey Bondarchuk <roislerxs@gmail.com>
2023-12-05 20:56:39 +0700

    feat: added category product

--- coffee-house/scripts.js
+++ coffee-house/scripts.js
@@ -1,12 +1,15 @@
-const showMenu = () => {
-  const burger = document.querySelector('.header-menu_burger');
-  const headerMenu = document.querySelector('.header-menu');
-  const navMenuOrigin = document.querySelector('.header-menu_nav');
-  const navMenu = navMenuOrigin.cloneNode(true);
-  const mobileMenu = document.createElement('div');
-  const logoMenuOrigin = document.querySelector('.header-menu_coffee-menu');
-  const logoMenu = logoMenuOrigin.cloneNode(true);
+const burger = document.querySelector('.header-menu_burger');
+const headerMenu = document.querySelector('.header-menu');
 
+const navMenuOrigin = document.querySelector('.header-menu_nav');
+const navMenu = navMenuOrigin.cloneNode(true);
+
+const mobileMenu = document.createElement('div');
+
+const logoMenuOrigin = document.querySelector('.header-menu_coffee-menu');
+const logoMenu = logoMenuOrigin.cloneNode(true);
+
+const showMenu = () => {
   burger.addEventListener('click', () => {
     if (burger.classList.contains('active')) {
       burger.classList.remove('active');
@@ -29,11 +32,13 @@ const showMenu = () => {
 showMenu();
 
 const slider = document.querySelector('.favorite-coffee_slider');
-const slides = Array.from(document.querySelectorAll('.favorite-coffee_slide'));
+const slides = Array.from(slider.querySelectorAll('.favorite-coffee_slide'));
 
 const buttonNext = document.querySelector('.control-next');
 const buttonPrev = document.querySelector('.control-prev');
+const buttonsStatus = document.querySelectorAll('.favorite-coffee_control');
 
+const buttonsCount = buttonsStatus.length;
 const slidesCount = slides.length;
 
 let slideIndex = 0;
@@ -64,11 +69,25 @@ const showSlide = () => {
       slide.classList.remove('active');
     }
   });
+
+  buttonsStatus.forEach((buttonStatus, index) => {
+    let buttonIndex = slideIndex;
+    if (index === buttonIndex) {
+      buttonStatus.classList.add('active');
+    } else {
+      buttonStatus.classList.remove('active');
+    }
+  })
 };
 
+slides.forEach((slide) => {
+  slide.addEventListener('swipe', (e) => {
+    e.preventDefault();
+    console.log(e.details)
+  })
+})
 buttonNext.addEventListener('click', showNextSlide);
 buttonPrev.addEventListener('click', showPrevSlide);
 
 showSlide();
 
-setInterval(showNextSlide, 5000);
